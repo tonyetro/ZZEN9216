f0bb8a84dda5871ad9f7982459afdfe0c616e04e
Tony Caelum <ssj.caelum@gmail.com>
2022-10-03 09:58:50 +1100

    birthday attack completed

--- Week5/birthdayAttack.c
+++ Week5/birthdayAttack.c
@@ -1,12 +1,17 @@
 #include "hashString.h"
 
+#define MAX_ATTEMPTS 9999999 // max unsigned int
+#define MAX_MATCHES 6
+
 char *hashBuilder(char *str, int attempt);
-void printLogic(char *newValue, char *targetHash,
+void printLogic(char *newValue, char *targetValue, char *targetHash,
                 int *mainCounter, int *matchCounter);
+void printToDisk(char *valueOne, char *valueTwo);
 
 int main(void) {
     int mainCounter = 0, matchCounter = 1;
-    char *targetHash = hash_string("z5411839 trollolololol", "sha1");
+    char *targetValue = "z5411839 trololololol";
+    char *targetHash = hashString(targetValue, "sha1");
 
     while (mainCounter < MAX_ATTEMPTS && matchCounter <= MAX_MATCHES) {
         char zId[256] = "z5411839";
@@ -17,7 +22,7 @@ int main(void) {
         fflush(stdout);
 
         // Pass in necessary params to print out correct matches 
-        printLogic(newValue, targetHash,
+        printLogic(newValue, targetValue, targetHash,
                    &mainCounter, &matchCounter);
 
         mainCounter = mainCounter + 1;
@@ -25,31 +30,35 @@ int main(void) {
 }
 
 char *hashBuilder(char *zId, int attempt) {
-    char result[] = " 0000000";
-    sprintf(result, " %07d", attempt);
+    char result[] = " 00000000";
+    sprintf(result, " %08d", attempt);
     strcat(zId, result);
-
     return zId;
 }
 
-void printLogic(char *newValue, char *targetHash,
+void printLogic(char *newValue, char *targetValue, char *targetHash,
                 int *mainCounter, int *matchCounter) {
-    char *newHash = hash_string(newValue, "sha1");
+    char *newHash = hashString(newValue, "sha1");
+
     // If match counter is successful, print hashes and
     // reset match counter until it maxes out
     if (strncmp(newHash, targetHash, *matchCounter) == 0) {
         if (*matchCounter == 1) {
-            printf("\nMatch for %d character:\n", *matchCounter);
+            printf("\nResult for %d matching character:\n", *matchCounter);
         } else {
-            printf("\nMatch for %d characters:\n", *matchCounter);
+            printf("\nResult for %d matching characters:\n", *matchCounter);
         }
-        printf("Value:\t%s\n"
-               "Hash:\t%s\n\n",
-               newValue, newHash);
 
-        *mainCounter = 0;
+        printf("Target Hash:\t%s | Value: %s\n"
+               "Found Hash:\t%s | Value: %s\n\n",
+               targetHash, targetValue, newHash, newValue);
 
         if (*matchCounter == MAX_MATCHES) {
+            // With final result, write to files on disk:
+            // input_1.txt and input_2.txt
+            printToDisk(newValue, targetValue);
+            
+            // Escape all loops and eventually the program
             *mainCounter = MAX_ATTEMPTS; // Escape all looping
         } else {
             *matchCounter = *matchCounter + 1;
@@ -57,4 +66,14 @@ void printLogic(char *newValue, char *targetHash,
     } else if (*mainCounter + 1 == MAX_ATTEMPTS) {
         printf("No match found.");
     }
+}
+
+void printToDisk(char *valueOne, char *valueTwo) {
+    FILE *inputFilePtrOne = fopen("input_1.txt", "w");
+    fprintf(inputFilePtrOne, "%s", valueOne);
+    fclose(inputFilePtrOne);
+    
+    FILE *inputFilePtrTwo = fopen("input_2.txt", "w");
+    fprintf(inputFilePtrTwo, "%s", valueTwo);
+    fclose(inputFilePtrTwo);
 }
--- Week5/hashString.h
+++ Week5/hashString.h
@@ -1,22 +1,17 @@
-/**
- * Tony Caleum
- * 19 September 22, ZZEN9216
- */
-
-// write input to a temp file,
-// generate the hash of the file using the specified method,
+// write input to a temp file, 
+// generate the hash of the file using the specified method, 
 // delete the file
 // return the hash
 
-#define TMP_FILENAME "HASH_STRING_TMP.txt"
-#define MAX_ATTEMPTS 500000
-#define MAX_MATCHES 2
+// 20 May Sascha Graham
 
 #include <stdio.h>
 #include <string.h>
 #include <openssl/evp.h>
 
-char *hash_string(char *toHash, char *method) {
+#define TMP_FILENAME "HASH_STRING_TMP.txt"
+
+char *hashString(char *toHash, char *method) {
     unsigned char buffer[BUFSIZ];
     FILE *f;
     size_t len;
@@ -34,7 +29,7 @@ char *hash_string(char *toHash, char *method) {
         printf("Error: unknown hash method: %s\n", method);
         exit(1);
     }
-
+    
     // create a tmp file to contain the inputted string
     f = fopen(TMP_FILENAME, "w+");
     if (f == NULL) {
@@ -46,22 +41,22 @@ char *hash_string(char *toHash, char *method) {
     fputs(toHash, f);
     rewind(f);
 
-    // compute the hash
+    // compute the hash    
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, md, NULL);
 
     do {
-        len = fread(buffer, 1, BUFSIZ, f);
-        EVP_DigestUpdate(mdctx, buffer, len);
-    } while (len == BUFSIZ);
-
+            len = fread(buffer, 1, BUFSIZ, f);
+            EVP_DigestUpdate(mdctx, buffer, len);
+        } while (len == BUFSIZ);
+    
     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_free(mdctx);
-
+    
     // format hash from hex to string
-    char *hash_as_string = malloc(sizeof(char) * md_len*2 + 1);
+    char *hashAsString = malloc(sizeof(char) * md_len*2 + 1);
     for (i = 0; i < md_len; i++) {
-        sprintf(&hash_as_string[i*2], "%02x", md_value[i]);
+        sprintf(&hashAsString[i*2], "%02x", md_value[i]);
     }
 
     // close and delete the tmp file
@@ -69,8 +64,8 @@ char *hash_string(char *toHash, char *method) {
     if (!remove(TMP_FILENAME) == 0) {
         printf("Error: tmp file couldn't be deleted\n");
     }
-
+    
     EVP_cleanup();
 
-    return hash_as_string;
+    return hashAsString;
 }
